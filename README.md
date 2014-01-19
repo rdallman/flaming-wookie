eb8c3f497d1d92e95c3bc75ffa7aa9f19137803a
Reed Allman <rdallman10@gmail.com>
2014-01-19 01:14:28 -0600

    exoskeleton
    basic auth in place, not hooked up w/ db
    basic routes and static file server
    example database query with pg
    few pages to get a feel

--- README.md
+++ README.md
@@ -3,3 +3,44 @@
 Senior Design Project at Aubrun University
 
 War Damn Sweet Tea
+
+### TODO
+
+make sure:
+
+get postgresql installed on your machine if you don't:
+
+`% brew install postgres`
+
+get dependencies:
+
+```
+% go get github.com/gorilla/mux
+% go get github.com/lib/pq
+```
+
+the low hanging fruit: 
+
+* drop tables on first go round (design first)
+* make html all purrty and make stylesheet for css and template partials like
+  nav, imports
+* cookies on login (gorilla has nice stuff for this...)
+* if no cookie, show login; if cookie, show mgmt stuff in nav
+* encapsulate some new .go files
+
+high hanging fruit:
+
+* hook up register (javascript validate w/ regex?)
+* make login actually work (and display if otherwise)
+* quiz pages (js laden? use cookie to figure out start/not ability)
+* admin panel (view/make classes & quizzes)
+* student "register" for class (URI scheme?)
+* JSON protocol for answering quiz
+* QR code generation / reading
+
+
+thought splurge: 
+
+select students from class join quiz using qid where id = $ID 
+for authentication of student for quiz... so quiz has FK classid?
+
---
+++ server.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"database/sql"
+	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"html/template"
+	"net/http"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "dbname=wookie")
+	if err != nil {
+		panic(err)
+	}
+	//TODO drop tables
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func loginPage(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "login.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+//TODO ...see a pattern here for *Page(w, r)
+func registerPage(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "register.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	email, pass := r.FormValue("email"), r.FormValue("password")
+	fmt.Fprintf(w, "<html><body><h1>Hello, %s %s!</h1></body></html>", email, pass)
+	//TODO check for authenticiousnessity
+	//TODO return site wide cookie & add to DB
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	email, pass := r.FormValue("email"), r.FormValue("password")
+
+	salt := make([]byte, 32)
+	_, err := rand.Read(salt)
+
+	//FIXME just to see if this worked
+	fmt.Fprintf(w, "%s %s %s", email, pass, salt)
+
+	//create secure, salted hash
+	p := append([]byte(pass), salt...)
+	hash := sha256.Sum256(p)
+
+	//TODO check existence?
+	_, err = db.Exec(`INSERT INTO teachers(email, password, salt)
+    VALUES($1, $2, $3)`, email, hash, salt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return //necessary?
+	}
+}
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	//FIXME stub to check for cookie
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/static/"))))
+	r.HandleFunc("/", homePage)
+	r.HandleFunc("/login", loginPage).Methods("GET")
+	r.HandleFunc("/logmein", login).Methods("POST")
+	r.HandleFunc("/register", registerPage).Methods("GET")
+	r.HandleFunc("/register", register).Methods("POST")
+
+	//TODO these are just ideas
+	//r.HandleFunc("/quiz/{id}", handleQuizGet).Methods("GET")
+	//r.HandleFunc("/quiz/{id}", handleAnswer).Methods("PUT")
+	//r.HandleFunc("/quiz/{id}/edit, handleQuizEdit).Methods("POST, GET")
+	//r.HandleFunc("/quiz/add, handleQuizCreate).Methods("POST")
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
