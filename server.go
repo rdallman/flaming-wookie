1918dba807df77c36b5068020e368eb0b69a12ca
Reed Allman <rdallman10@gmail.com>
2014-01-21 00:21:18 -0600

    added infrastucture for restructure
    few issues with types, missing columns and table names; nbd
    added proper hash comparison for login

--- server.go
+++ server.go
@@ -35,7 +35,49 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+	//TODO NEED TO USE BYTEA TO STORE HASHES
+	//_, err = db.Exec(`ALTER TABLE "Users" ALTER COLUMN password TYPE bytea`)
+	//fmt.Println(err)
+	//_, err = db.Exec(`ALTER TABLE "Users" ALTER COLUMN salt TYPE bytea`)
+	//fmt.Println(err)
 	// TODO drop tables
+	// DANGER this will empty the db
+	// I'm going to do this just to change the names because it's a pain in the ass
+	// and nice for testing (and reading types and such, to boot)
+
+	//_, err = db.Exec(`DROP TABLE "Classes" "Users" "Students"`)
+	//fmt.Println(err)
+
+	//rows, err := db.Query("SELECT tablename from pg_catalog.pg_tables")
+	//for rows.Next() {
+	//var tablename string
+	//rows.Scan(&tablename)
+	//fmt.Println(tablename)
+	//}
+	//_, err = db.Exec(`CREATE TABLE teachers
+	//(tid integer,
+	//email text,
+	//password text,
+	//salt text)`)
+
+	//_, err = db.Exec(`CREATE TABLE classes
+	//(cid integer,
+	//name text,
+	//active boolean,
+	//uid text)`) //TODO not sure about this one?
+
+	////TODO this just feels wrong
+	//_, err = db.Exec(`CREATE TABLE students
+	//(sid text,
+	//pin integer,
+	//cid integer)`)
+
+	//_, err = db.Exec(`CREATE TABLE quiz
+	//(qid integer,
+	//name text,
+	//info json)`)
+
+	//TODO solution for grading
 }
 
 func handlePage(name string) func(http.ResponseWriter, *http.Request) {
@@ -53,18 +95,25 @@ func handlePage(name string) func(http.ResponseWriter, *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	//r.ParseForm()
 	inputEmail, inputPass := r.FormValue("email"), r.FormValue("password")
-	// TODO check for authenticiousnessity
-	var pw string
-	err := db.QueryRow("SELECT password FROM \"Users\" WHERE username=$1", inputEmail).Scan(&pw)
+	fmt.Fprintf(w, inputPass, inputEmail)
+
+	//dbpw is the salted sha256 hash we stored as password
+	var salt, dbpw string
+	err := db.QueryRow(`SELECT salt, password FROM "Users" WHERE username=$1`, inputEmail).Scan(&salt, &dbpw)
+
+	//salt input password, hash and compare to database salted hash
+	hash := sha256.Sum256(append([]byte(inputPass), salt...))
+	phash := string(hash[:]) //finicky
+
 	switch {
 	case err == sql.ErrNoRows:
 		// TODO add flash messages
 		fmt.Fprintf(w, "No user with that username.")
 	case err != nil:
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-	case inputPass != pw:
+	case dbpw != phash:
 		// TODO add flash messages
 		fmt.Fprintf(w, "Incorrect password")
 	default:
@@ -86,24 +135,31 @@ func register(w http.ResponseWriter, r *http.Request) {
 
 	salt := make([]byte, 32)
 	_, err := rand.Read(salt)
-
-	// FIXME just to see if this worked
-	fmt.Fprintf(w, "%s %s %s", email, pass, salt)
+	//saltstr := string(salt[:])
 
 	// create secure, salted hash
-	p := append([]byte(pass), salt...)
-	hash := sha256.Sum256(p)
+	hash := sha256.Sum256(append([]byte(pass), salt...))
+	phash := string(hash[:])
 
 	// TODO check existence?
-	_, err = db.Exec(`INSERT INTO teachers(email, password, salt)
-    VALUES($1, $2, $3)`, email, hash, salt)
+	_, err = db.Exec(`INSERT INTO "Users" (username, password, salt)
+    VALUES($1, $2, $3)`, email, phash, salt)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return // necessary?
 	}
+	http.Redirect(w, r, "/login", 307)
 }
 
 // checks for cookie; if no cookie then redirect to home page
+//
+// this function... I don't think it does what you think it does
+// I am Inigo Montoya. You kill my father. Prepare to die.
+//
+// should be done on every request (sigh, I know) and display different options for
+// teachers, alternatively you could return a user each time and if not nil, you have a teacher
+// then with that knowledge, get teacher specific data from database (in other methods...).
+// going off of that, I guess you really just need to return an ID each time since that'll be
+// what's used to hit the DB. That may prove problematic but premature optimization is the root of all... well, you know
 func auth(w http.ResponseWriter, r *http.Request) error {
 	// FIXME stub to check for cookie
 	_, err := r.Cookie("logged-in")
