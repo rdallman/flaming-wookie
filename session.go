f4c186e4e0bf94c32edce8b7ea5f92b28ae1820e
Reed Allman <rdallman10@gmail.com>
2014-02-09 18:54:10 -0600

    add error to success: false

--- session.go
+++ session.go
@@ -14,7 +14,7 @@ import (
 func writeErr(err error, w http.ResponseWriter) {
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(w, Response{"success": false})
+		fmt.Fprint(w, Response{"success": false, "message": err.Error()})
 	}
 }
 
