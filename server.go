9892846df13caaa89a7f7a620848ca3043e2d221
Reed Allman <rdallman10@gmail.com>
2014-02-19 10:58:11 -0600

    update urls, tests

--- server.go
+++ server.go
@@ -54,10 +54,12 @@ func main() {
 	r.HandleFunc("/classes", handleClassList).Methods("GET")
 	r.HandleFunc("/classes", handleCreateClass).Methods("POST")
 	//TODO r.HandleFunc("/classes/{cid:[0-9]+}", handleClassGet).Methods("GET")
+	//TODO r.HandleFunc("/classes/{cid:[0-9]+}", handleClassUpdate).Methods("POST")
+	//TODO r.HandleFunc("/classes/{cid:[0-9]+}", handleClassDelete).Methods("DELETE")
 	r.HandleFunc("/classes/{cid:[0-9]+}/quiz", handleQuizList).Methods("GET")
 	r.HandleFunc("/classes/{cid:[0-9]+}/quiz", handleQuizCreate).Methods("POST")
 	r.HandleFunc("/classes/{cid:[0-9]+}/student", handleAddStudents).Methods("POST")
-	//TODO /classes/{cid:[0-9]+}/students POST, DELETE (i.e. update student, delete student)
+	//TODO /classes/{cid:[0-9]+}/students POST, DELETE (i.e. update student, delete student) //Do we want this? just update class?
 
 	//API quiz methods (api.go)
 	r.HandleFunc("/quiz", handleQuizList).Methods("GET")
--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ echo -e "2\n" && curl -X GET -b $cookies http://localhost:8080/classes
 echo -e "3\n" && curl -X GET -b $cookies http://localhost:8080/classes/11/quiz
 #curl -X POST -d "{\"title\":\"This is my quiz\",\"cid\":11,\"info\": {\"questions\":[{\"text\":\"Are there many like it?\",\"answers\":[\"Yay\",\"Nay\"],\"correct\":0}],\"grades\":{\"9025038951111\":0}}}" -b $cookies http://localhost:8080/classes/11/quiz
 #curl -X POST -d "{\"name\":\"Maghen\", \"sid\":\"1\"}" -b $cookies http://localhost:8080/classes/11/student
-echo -e "4\n" && curl -X GET -b $cookies http://localhost:8080/quiz/75
+echo -e "4\n" && curl -X GET -b $cookies http://localhost:8080/quiz/71
 echo -e "5\n" && curl -X PUT -d "{\"state\":0}" -b $cookies http://localhost:8080/quiz/71/state
 echo -e "6\n" && curl -X PUT -d "{\"id\":\"12345\", \"answer\":0}" http://localhost:8080/quiz/71/answer
 
