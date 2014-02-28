334cf8c0ab0ea8469942baa76bed39e18a32f4ba
Reed Allman <rdallman10@gmail.com>
2014-02-28 17:14:55 -0600

    small tweaks to make sessions serviceable
    changed id to use more efficient keying
    state sends in json from angular now
    added qid to quiz splash page

--- email.go
+++ email.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"net/smtp"
 	"bytes"
-	"text/template"
 	"log"
+	"net/smtp"
 	"strconv"
+	"text/template"
 
 	_ "github.com/lib/pq"
 )
 
 type EmailUser struct {
-  Username    string
-  Password    string
-  EmailServer string
-  Port        int
+	Username    string
+	Password    string
+	EmailServer string
+	Port        int
 }
 
 var emailUser = &EmailUser{"wookiet3st", "wooquiz1", "smtp.gmail.com", 587}
 
 var emailauth = smtp.PlainAuth("",
-  emailUser.Username,
-  emailUser.Password,
-  emailUser.EmailServer,
+	emailUser.Username,
+	emailUser.Password,
+	emailUser.EmailServer,
 )
 
 type SmtpTemplateData struct {
-  From	  string
-  Subject string
-  Body    string
+	From    string
+	Subject string
+	Body    string
 }
 
 const emailTemplate = `From: {{.From}}
@@ -42,36 +42,36 @@ Sincerely,
 
 func sendStudentClassEmail(cid int, classname string, student map[string]string) {
 	var err error
-	
+
 	//create text for email
 	var doc bytes.Buffer
 	context := SmtpTemplateData{
-	  "WooQuiz",
-	  classname + " Class Registration",
-	  "Hello " + student["fname"] + " " + student["lname"] + "," +
-	  	"\n\nYou have been added to the class " + classname + " on WooQuiz.com!" +
-	  	"\nClass ID: " + strconv.Itoa(cid) + 
-	  	"\nStudent ID: " + student["sid"],
+		"WooQuiz",
+		classname + " Class Registration",
+		"Hello " + student["fname"] + " " + student["lname"] + "," +
+			"\n\nYou have been added to the class " + classname + " on WooQuiz.com!" +
+			"\nClass ID: " + strconv.Itoa(cid) +
+			"\nStudent ID: " + student["sid"],
 	}
 
 	//template email
 	t := template.New("emailTemplate")
 	t, err = t.Parse(emailTemplate)
 	if err != nil {
-	  log.Print("error trying to parse mail template")
+		log.Print("error trying to parse mail template")
 	}
 	err = t.Execute(&doc, context)
 	if err != nil {
-	  log.Print("error trying to execute mail template")
+		log.Print("error trying to execute mail template")
 	}
 
 	//send email
 	err = smtp.SendMail(emailUser.EmailServer+":"+strconv.Itoa(emailUser.Port),
-	  emailauth,
-	  emailUser.Username,
-	  []string{student["email"]},
-	  doc.Bytes())
+		emailauth,
+		emailUser.Username,
+		[]string{student["email"]},
+		doc.Bytes())
 	if err != nil {
-	  log.Print("ERROR: attempting to send a mail ", err)
+		log.Print("ERROR: attempting to send a mail ", err)
 	}
 }
