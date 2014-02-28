0850d9b849443ee3331574192c8830f8aa21cdd8
Robert Horn <rehorn09@gmail.com>
2014-02-28 08:12:07 -0600

    working on edit class feature, some refactorings

--- static/app/controllers/classCtrl.js
+++ static/app/controllers/classCtrl.js
@@ -4,10 +4,6 @@ var classApp = angular.module('classControllers', ['ngRoute']);
 
 classApp.controller('ClassController', function ($scope, $http, $route, $routeParams, $location) {
 
-  $scope.myClass = {
-    name: "",
-    students: []
-  };
   $scope.classes = [];
   $scope.id = -1; 
 
@@ -16,28 +12,35 @@ classApp.controller('ClassController', function ($scope, $http, $route, $routePa
     students: []
   }
 
-  $http({
-    method: 'GET',
-    url: '/classes'
-  }).
-  success(function(data) {
-  console.log(data);  
-    angular.forEach(data["info"], function(value, key) {
-      this.push(value)
-    }, $scope.classes);
-  }).
-  error(function(data) {
-    // handle
-  });
-
-  if ($routeParams.id !== undefined) {	
+  // class list
+  if ($routeParams.id === undefined) {
+    $http({
+      method: 'GET',
+      url: '/classes'
+    }).
+    success(function(data) {
+    console.log(data);  
+      angular.forEach(data["info"], function(value, key) {
+        this.push(value)
+      }, $scope.classes);
+    }).
+    error(function(data) {
+      // handle
+    });
+  }
+  // specific class
+  else {
     $http({
       method: 'GET',
       url: '/classes/' + $routeParams.id
     }).success(function(data) {
       if (data !== undefined) {
-      $scope.myClass = data["info"]
-      $scope.id = $routeParams.id;
+        $scope.class = data["info"]
+        $scope.id = $routeParams.id;
+        // we're editing the class
+        // TODO better way to check path that has id param...
+        if ($location.$$path.match(/(\/classes\/[0-9]+\/edit)/)) {
+        }
       }
     }).error(function(data) {
       // handle error
@@ -45,31 +48,48 @@ classApp.controller('ClassController', function ($scope, $http, $route, $routePa
   }
 
   $scope.createClass = function() {
-      //alert(angular.toJson($scope.class));
-    $http({
-      method: 'POST',
-      url: '/classes',
-      data: angular.toJson($scope.class),
-      headers: {'Content-Type': 'application/json'}
-    });
-    $location.path('/main');
-    
+    if ($location.$$path.match(/(\/classes\/[0-9]+\/edit)/)) {
+
+    }
+    else {
+      
+      $http({
+        method: 'POST',
+        url: '/classes',
+        data: angular.toJson($scope.class),
+        headers: {'Content-Type': 'application/json'}
+      });
+      $location.path('/main');
+    }
   }
 
   $scope.addStudent = function(email, firstName, lastName) {
-    $scope.class.students.push({email: email, fname: firstName, lname: lastName});
-    $scope.student.email = "";
-    $scope.student.fname = "";
-    $scope.student.lname = "";
+    if ($location.$$path.match(/(\/classes\/[0-9]+\/edit)/)) {
+      $http({
+        method: 'POST',
+        url: '/classes/' + $routeParams.id + '/student',
+        data: angular.toJson({cid: $routeParams.id, email: email, fname: firstName, lname: lastName}),
+        headers: {'Content-Type': 'application/json'}
+      }).success(function(data) {
+        $scope.class.students.push({email: email, name: {first: firstName, last: lastName}}); 
+      }).error(function(data) {
+        alert("Error: Could not add student");
+      });
+    }
+    else {
+      $scope.class.students.push({email: email, fname: firstName, lname: lastName});
+      $scope.student.email = "";
+      $scope.student.fname = "";
+      $scope.student.lname = "";    }
 
   }
 
   
 
   $scope.changeName = function(text) {
-    $scope.myClass.name = text;
+    $scope.class.name = text;
   }
   $scope.removeStudent = function(student) {
-    $scope.myClass.students.splice($scope.myClass.students.indexOf(student), 1);
+    $scope.class.students.splice($scope.class.students.indexOf(student), 1);
   }
 });
