f45294d6d30f49625aa0af4d755b8ab495cc5084
Reed Allman <rdallman10@gmail.com>
2014-04-15 00:32:51 -0400

    added daemon script

---
+++ daemon.bash
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#
+# Linux start stop script
+#
+
+# Package
+DNAME="flaming-wookie"
+PNAME="wookie_daemon"
+
+# Others
+# current path resolver from http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+INSTALL_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+PROGRAM="${GOPATH}/bin/${DNAME}"
+PID_FILE="/var/${PNAME}.pid"
+
+
+start_daemon ()
+{
+    cd "${INSTALL_DIR}"
+    ${PROGRAM} --pidfile ${PID_FILE} &
+}
+
+stop_daemon ()
+{
+    kill `cat ${PID_FILE}`
+    wait_for_status 1 20 || kill -9 `cat ${PID_FILE}`
+    rm -f ${PID_FILE}
+}
+
+daemon_status ()
+{
+    if [ -f ${PID_FILE} ] && kill -0 `cat ${PID_FILE}` > /dev/null 2>&1; then
+        return
+    fi
+    rm -f ${PID_FILE}
+    return 1
+}
+
+wait_for_status ()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        [ $? -eq $1 ] && return
+        let counter=counter-1
+        sleep 1
+    done
+    return 1
+}
+
+# Now do what you need to do.
+
+case $1 in
+    start)
+        if daemon_status; then
+            echo ${DNAME} is already running
+        else
+            echo Starting ${DNAME}...
+            start_daemon
+        fi
+        ;;
+    stop)
+        if daemon_status; then
+            echo Stopping ${DNAME}...
+            stop_daemon
+        else
+            echo ${DNAME} is not running
+        fi
+        ;;
+    status)
+        if daemon_status; then
+            echo ${DNAME} is running
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
+        ;;
+    restart)
+        $0 stop && sleep 2 && $0 start && sleep 2 && $0 status
+        ;;
+    *)
+        exit 1
+        ;;
+esac
